c6176b1269105b0b1a6516c934b81752809a159d
epuyao-ruben <ruben.epuyao@gmail.com>
2024-02-11 20:30:02 -0300

    update some changes

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,8 +6,19 @@ let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
+    temples.forEach((temple) => {
+        const article = document.createElement("article");
+        const h3 = document.createElement("h3");
+        const img = document.createElement("img");
+        h3.textContent = temple.templeName;
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+        article.appendChild(h3);
+        article.appendChild(img);
+        templesElement.appendChild(article);
+    });
+};
 
-} 
 
 
 
