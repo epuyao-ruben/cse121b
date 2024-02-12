4a2247536c7f597dee4b9f3c27a441d843a9ea9d
epuyao-ruben <ruben.epuyao@gmail.com>
2024-02-11 22:20:00 -0300

    updated

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,11 +6,11 @@ let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    temples.forEach((temple) => {
-        const article = document.createElement("article");
-        const h3 = document.createElement("h3");
-        const img = document.createElement("img");
+    temples.forEach(temple => {
+        let article = document.createElement("article");
+        let h3 = document.createElement("h3");
         h3.textContent = temple.templeName;
+        let img = document.createElement("img");
         img.src = temple.imageUrl;
         img.alt = temple.location;
         article.appendChild(h3);
@@ -20,24 +20,61 @@ const displayTemples = (temples) => {
 };
 
 
-
-
 /* async getTemples Function using fetch()*/
-temples.forEach(templesElement => {
-    let article = document.createElement("article");
-    let h3 = document.createElement("h3");
-    h3.textContent = templeName;
-    let img = document.createElement("img");
-    img.textContent = imageUrl;
-    
-});
 
-/* reset Function */
+
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templeList = await response.json();
+    displayTemples(templeList);
+    console.log(templeList);
+
+    /* reset Function */
+    const reset = () => {
+        templesElement.innerHTML = "";
+    };
+}
 
 
 /* filterTemples Function */
+const filterTemples = (temples) => {
+    reset();
+    const filter = document.querySelector("#filtered").value;
+    switch (filter) {
+        case "utah":
+            displayTemples(temples.filter((temple) => temple.location.includes("Utah")));
+            break;
+        case "nonutah":
+            displayTemples(temples.filter((temple) => !temple.location.includes("Utah")));
+            break;
+        case "older":
+            displayTemples(temples.filter((temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case "alphabetic":
+            displayTemples(
+                temples.sort((a, b) => {
+                    let fa = a.templeName.toLowerCase(),
+                        fb = b.templeName.toLowerCase();
+
+                    if (fa < fb) {
+                        return -1;
+                    }
+                    if (fa > fb) {
+                        return 1;
+                    }
+                    return 0;
+                })
+            );
+        case "all":
+        default:
+            displayTemples(temples);
+            break;
+    }
+};
 
 
-getTemples();
 
 /* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => { filterTemples(templeList) });
+
+getTemples();
